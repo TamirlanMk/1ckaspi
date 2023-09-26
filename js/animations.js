f632ef3ce646ff6becac33330617521441780be1
Tamirlan Mukashev <tamirlan_mukashev@mail.ru>
2023-09-27 03:10:49 +0600

    feat: add animations with gsap

--- js/animations.js
+++ js/animations.js
@@ -1,23 +1,120 @@
-gsap.utils.toArray(".advantages__cards-item").forEach(function(card) {
-    gsap.set(card, {
-        transformStyle: "preserve-3d",
-    });
-    const q = gsap.utils.selector(card);
-    const front = q(".advantages__cards-item__front");
-    const back = q(".advantages__cards-item__back");
+gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(TextPlugin);
 
-    gsap.set(back, { rotationY:-180 });
+const text = [
+    'Увеличение конверсии продаж',
+    'Улучшение эффективности рабочего времени',
+    'Улучшение качества обслуживания клиентов',
+    'Порядок на складе и в программе',
+    'Снижение риска ошибок',
+]
 
-    const tl = gsap.timeline({ paused: true })
-        .to(front, { duration: 0.6, rotationY: 180 })
-        .to(back, { duration: 0.6, rotationY: 0 }, 0)
-        .to(card, { z: 50 }, 0)
-        .to(card, { z: 0 }, 0.5);
+// Laptop Animations
+let laptopScreen = window.matchMedia('(min-width:992px)')
 
-    card.addEventListener("mouseenter", function() {
-        tl.play();
+if (laptopScreen.matches) {
+
+    let tlHero = gsap.timeline({
+        toggleActions: 'play none none reverse'
     });
-    card.addEventListener("mouseleave", function() {
-        tl.reverse();
+
+    tlHero.add('hero__image').from('.hero__image-1', {
+        yPercent: 100,
+        duration: 1.5
+    }, {}, 'hero__image').from('.hero__image-2', {
+        yPercent: -100,
+        duration: 1.5
+    }, {}, 'hero__image')
+
+    let tlAdvantages = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".advantages",
+            start: '-100px center',
+            toggleActions: 'play none none reverse',
+        }
     });
-});
+
+    for (let i = 1; i <= 4; i++) {
+        tlAdvantages
+            .fromTo(`.advantages__grid-item:nth-child(${i})`, {
+                opacity: 0,
+                xPercent: i % 2 ? -100 : 100,
+            }, {
+                opacity: 1,
+                xPercent: 0,
+                duration: 0.4
+            }, '>')
+    }
+
+    let tlAboutWork = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".about-work",
+            start: '-100px center',
+            toggleActions: 'play none none reverse',
+        }
+    });
+
+    for (let i = 1; i <= 4; i++) {
+        tlAboutWork
+            .fromTo(`.about-work__item:nth-child(${i}) .about-work__item-title`, {
+                color: '#fff'
+            }, {
+                color: '#3db3e4',
+                duration: 1
+            }, '<')
+            .fromTo(`.about-work__item:nth-child(${i}) .about-work__item-img`, {
+                xPercent: i % 2 ? 100 : -100,
+                autoAlpha: 0
+            }, {
+                xPercent: 0,
+                autoAlpha: 1,
+                duration: 0.6
+            }, '<')
+            .fromTo(`.about-work__item:nth-child(${i}) .about-work__item-description`, {
+                color: '#fff'
+            }, {
+                color: '#000',
+                duration: 1
+            }, '<')
+            .fromTo(`.about-work__item:nth-child(${i}) .after-line`, {
+                width: '0px'
+            }, {
+                width: '100%',
+                delay: .25,
+                duration: 1
+            }, '<')
+            .fromTo(`.about-work__item:nth-child(${i}) .before-line`, {
+                width: '0px'
+            }, {
+                width: '100%',
+                duration: 1
+            }, '<')
+    }
+
+    let tlProsIntegration = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".pros-integration",
+            start: '-100px center',
+            toggleActions: 'play none none reverse',
+        }
+    })
+
+    for (let i = 1; i <= 5; i++) {
+        tlProsIntegration
+            .fromTo(`.pros-integration__item:nth-child(${i}) .pros-integration__item-number`, {
+                opacity: 0,
+                xPercent: -100
+            }, {
+                opacity: 1,
+                xPercent: 0,
+                duration: 0.4
+            }, '>')
+            .to(`.pros-integration__item:nth-child(${i}) .pros-integration__item-text`, {
+                    text: {
+                        value: text[i - 1],
+                    },
+                    duration: 1
+                }, '<'
+            )
+    }
+}
